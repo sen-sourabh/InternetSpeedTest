3204c7ff95c21112c2f36d1113627bb354bd9257
Sourabh Sen <sourabhsen201313@gmail.com>
2021-02-28 14:18:51 +0530

    Initial Commit with InternetSpeed working

---
+++ ist/src/components/InternetSpeed/networkinfo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <detected-speed @network-type="handleNetworkIdentified" @network-speed="handleNetworkSpeed">
+            <span slot="unknown">
+                <div>{{ network }}/{{ speed }} Mbps <q-icon size="xs" name="trending_flat" /></div>
+            </span>
+            <span slot="slow">
+                <div>{{ network }}/{{ speed }} Mbps <q-icon size="xs" name="arrow_downward" /></div>
+            </span>
+            <span slot="fast">
+                <div>{{ network }}/{{ speed }} Mbps <q-icon size="xs" name="arrow_upward" /></div>
+            </span>
+        </detected-speed>
+    </div>
+</template>
+
+<script>
+import DetectedSpeed from 'vue-identify-network';
+
+export default {
+    name: "NetworkInfo",
+    components: {
+        DetectedSpeed
+    },
+    data() {
+        return {
+            network: 0,
+            speed: 0
+        }
+    },
+    methods: {
+        handleNetworkIdentified(type) {
+            this.network = type;
+            console.log('connection type: ', type);
+        },
+        handleNetworkSpeed(speed) {
+            this.speed = speed;
+            console.log('downlink: ', speed);
+        }
+    }
+};
+</script>
---
+++ ist/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <q-page class="flex flex-center">
+      <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-full.svg"
+      >
+    </q-page>
+    <h2>Home</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
